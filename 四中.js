2f71338fab64a762ecaffc5be4664daff16d0a2c
GHSRobert Ciang <Robertus0617@gmail.com>
2025-03-03 00:12:58 +0800

    總算弄出可依 radio button 選取類別一次產生一類的表格

--- 四中.js
+++ 四中.js
@@ -1,5 +1,7 @@
 function csvToArray(str, delimiter = ",") { // https://github.com/codewithnathan97/javascript-csv-array-example/blob/master/index.html
 
+  str = str.replace(/\r/g,""); // GHSRobert 自己加的，原本弄的會在行尾跑出 \r
+
   // slice from start of text to the first \n index
   // use split to create an array from string by delimiter
   const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
@@ -26,16 +28,33 @@ function csvToArray(str, delimiter = ",") { // https://github.com/codewithnathan
   return arr;
 }
 
+
+//cat = "2心理活動與感覺";
+//console.log(cat);
+
 function generate() {
+  var cat;
+  // Select all inputs with name="category"
+  var radios = document.querySelectorAll("input[name=\"category\"]");
+
+  // Use Array.forEach to add an event listener to each radio element.
+  radios.forEach(function(radio) {
+    radio.addEventListener('change', function() {
+      cat = document.querySelector('input[name="category"]:checked').value;
+      console.log(cat);
+    })
+  });
+
   var contentContainer = document.getElementById("generated");
-  const n = 85;
+  /*const n = 85;
   for (let i = 1; i < n+1; i++) {
     if (i <= 9) {i = "0"+i;}
     if (i <= 99) {i = "0"+i;}
     var p = document.createElement("p");
     p.innerHTML = i + " 詞&nbsp;<audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/w/1si-01-" + i + ".mp3' type='audio/mpeg'></audio>" + " 句&nbsp;<audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/s/1si-01-" + i + "s.mp3' type='audio/mpeg'></audio>";
     contentContainer.appendChild(p);
-  }
+  }*/
+
   
   document.getElementById('fileInput').addEventListener('change', function (event) {
     const file = event.target.files[0];
@@ -45,33 +64,42 @@ function generate() {
         const content = e.target.result;
         // document.getElementById('output').innerText = content;
         const data = csvToArray(content);
-        // var raw = document.createElement("p");
-        // raw.innerHTML = JSON.stringify(data); // 測試 okay 了
-        // contentContainer.appendChild(raw);
+        //var raw = document.createElement("p");
+        //raw.innerHTML = JSON.stringify(data); // 測試 okay 了
+        //contentContainer.appendChild(raw);
 
         var table = document.createElement("table");
-        for (i=0; i<=data.length; i++) {
-          //var 句 = data[i].四縣例句.replace(/"/g,'').replace(/\\n/g,'<br>');
-          //var 譯 = data[i].四縣翻譯.replace(/"/g,'').replace(/\\n/g,'<br>');
-          /* var item = document.createElement("tr");
-          var no = document.createElement("td");
-          var 句 = document.createElement("td");
-          var 譯 = document.createElement("td");
-          no.innerHTML = data[i].編號;
-          句.innerHTML = data[i].四縣例句.replace(/"/g,'').replace(/\\n/g,'<br>');
-          譯.innerHTML = data[i].四縣翻譯.replace(/"/g,'').replace(/\\n/g,'<br>');
-          item.appendChild(no);
-          item.appendChild(句);
-          item.appendChild(譯);
 
-          table.appendChild(item);*/
-          var no = data[i].編號.split("-");
-          if (no[0] <= 9) {no[0] = "0"+no[0];}
-          if (no[1] <= 9) {no[1] = "0"+no[1];}
-          if (no[1] <= 99) {no[1] = "0"+no[1];}
-          var item = document.createElement("tr");
-          item.innerHTML = "<td>" + data[i].編號 + "</td><td>" + data[i].四縣例句.replace(/"/g, '').replace(/\\n/g, '<br>') + "</td><td>" + data[i].四縣翻譯.replace(/"/g, '').replace(/\\n/g, '<br>') + "</td><td><audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/w/1si-" + no[0]+"-"+no[1] + ".mp3' type='audio/mpeg'></audio>" + " 句&nbsp;<audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/s/1si-" + no[0]+"-"+no[1] +"s.mp3' type='audio/mpeg'></audio></td>";
-          contentContainer.appendChild(item);
+        // var cat = document.querySelector('input[name="category"]:checked');
+        //for (i=0; i<=data.length; i++) {
+        for (const line of data) {
+
+          if (line.分類 === cat) {
+
+            //var 句 = data[i].四縣例句.replace(/"/g,'').replace(/\\n/g,'<br>');
+            //var 譯 = data[i].四縣翻譯.replace(/"/g,'').replace(/\\n/g,'<br>');
+            /* var item = document.createElement("tr");
+            var no = document.createElement("td");
+            var 句 = document.createElement("td");
+            var 譯 = document.createElement("td");
+            no.innerHTML = data[i].編號;
+            句.innerHTML = data[i].四縣例句.replace(/"/g,'').replace(/\\n/g,'<br>');
+            譯.innerHTML = data[i].四縣翻譯.replace(/"/g,'').replace(/\\n/g,'<br>');
+            item.appendChild(no);
+            item.appendChild(句);
+            item.appendChild(譯);
+
+            table.appendChild(item);*/
+            //var no = data[i].編號.split("-");
+            var no = line.編號.split("-");
+            if (no[0] <= 9) {no[0] = "0"+no[0];}
+            if (no[1] <= 9) {no[1] = "0"+no[1];}
+            if (no[1] <= 99) {no[1] = "0"+no[1];}
+            var item = document.createElement("tr");
+            //item.innerHTML = "<td>"+no[0]+"-"+no[1]+line.分類+"</td>";
+            item.innerHTML = "<td>" + line.編號 + "</td><td>" + line.四縣例句.replace(/"/g, '').replace(/\\n/g, '<br>') + "</td><td>" + line.四縣翻譯.replace(/"/g, '').replace(/\\n/g, '<br>') + "</td><td><audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/w/1si-" + no[0]+"-"+no[1] + ".mp3' type='audio/mpeg'></audio>" + " 句&nbsp;<audio class='media' controls='controls' preload='none' > <source src='https://elearning.hakka.gov.tw/hakka/files/cert/vocabulary/110/2/si/s/1si-" + no[0]+"-"+no[1] +"s.mp3' type='audio/mpeg'></audio></td>";
+            contentContainer.appendChild(item);
+          } else {continue;}
         }
         contentContainer.appendChild(table);
       };
